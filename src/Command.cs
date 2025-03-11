a110eb07d67387e2668d6759931f87a80a54a182
rubenebels23 <rubenebels2006@gmail.com>
2025-03-11 10:26:56 +0100

    thirds command word

--- src/Command.cs
+++ src/Command.cs
@@ -2,13 +2,18 @@ class Command
 {
 	public string CommandWord { get; init; }
 	public string SecondWord { get; init; }
-	
+
+	public string ThirdWord { get; init; }
+
+
 	// Create a command object. First and second word must be supplied, but
 	// either one (or both) can be null. See Parser.GetCommand()
-	public Command(string first, string second)
+	public Command(string first, string second, string third)
 	{
 		CommandWord = first;
 		SecondWord = second;
+		ThirdWord = third;
+
 	}
 
 	// Return true if this command was not understood.
@@ -21,5 +26,10 @@ class Command
 	public bool HasSecondWord()
 	{
 		return SecondWord != null;
+	}
+	// Return true if the command has a third word.
+	public bool HasThirdWord()
+	{
+		return ThirdWord != null;
 	}
 }
--- src/Parser.cs
+++ src/Parser.cs
@@ -3,7 +3,7 @@ using System;
 class Parser
 {
 	// Holds all valid command words
-	private readonly CommandLibrary commandLibrary; 
+	private readonly CommandLibrary commandLibrary;
 
 	// Constructor
 	public Parser()
@@ -18,19 +18,22 @@ class Parser
 
 		string word1 = null;
 		string word2 = null;
+		string word3 = null;
 
 		// string.Split() returns an array
 		string[] words = Console.ReadLine().Split(' ');
 		if (words.Length > 0) { word1 = words[0]; }
 		if (words.Length > 1) { word2 = words[1]; }
+		if (words.Length > 2) { word3 = words[2]; }
 
 		// Now check whether this word is known. If so, create a command with it.
-		if (commandLibrary.IsValidCommandWord(word1)) {
-			return new Command(word1, word2);
+		if (commandLibrary.IsValidCommandWord(word1))
+		{
+			return new Command(word1, word2, word3);
 		}
 
 		// If not, create a "null" command (for unknown command).
-		return new Command(null, null);
+		return new Command(null, null, null);
 	}
 
 	// Prints a list of valid command words from commandLibrary.
